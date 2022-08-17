984fb501712515d01ccc35b026eddb3dd34bb23b
JinnFighter <chernayamolnia@yandex.ru>
2022-08-17 12:12:46 +0300

    add: RoadTiles

--- Assets/Scripts/Start/GameInstaller.cs
+++ Assets/Scripts/Start/GameInstaller.cs
@@ -9,6 +9,9 @@ namespace Start
     {
         [SerializeField] private ViewContainer _viewContainer;
         [SerializeField] private PlayerView _playerView;
+        [SerializeField] private RoadStartTileView _roadStartTileView;
+        [SerializeField] private RoadMiddleTileView _roadMiddleTileView;
+        [SerializeField] private RoadFinishTileView _roadFinishTileView;
         [SerializeField] private UiView _uiView;
         
         public override void InstallBindings()
@@ -32,6 +35,9 @@ namespace Start
         {
             Container.Bind<ViewContainer>().FromInstance(_viewContainer).AsSingle();
             Container.Bind<PlayerView>().FromInstance(_playerView).AsSingle();
+            Container.Bind<RoadStartTileView>().FromInstance(_roadStartTileView).AsSingle();
+            Container.Bind<RoadMiddleTileView>().FromInstance(_roadMiddleTileView).AsSingle();
+            Container.Bind<RoadFinishTileView>().FromInstance(_roadFinishTileView).AsSingle();
             Container.Bind<UiView>().FromInstance(_uiView).AsSingle();
         }
     }
